fd66718d9c1d6b69ed803f785bbc66db2e1191c7
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2023-02-23 19:13:06 -0400

    navbar title responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { FaGithub } from "react-icons/fa";
 import { VscChevronRight, VscMenu,VscChromeMaximize, VscChromeMinimize, VscChromeClose, VscSplitHorizontal, VscTerminalTmux, VscSplitVertical } from "react-icons/vsc";
 import { GoKebabHorizontal } from "react-icons/go";
@@ -8,7 +8,41 @@ import useWindowSize from "../hooks/useWindowSize";
 function Nav() {
     const [isNavExpanded, setIsNavExpanded] = useState(false)
     const [isKebabExpanded, setIsKebabExpanded] = useState(false)
+    const [text,setText] = useState("Nav.jsx - portafolio - Visual Studio Code")
+    const textAux="Nav.jsx - portafolio - Visual Studio Code"
+    const textEl = useRef(null);
+
     const size = useWindowSize();
+
+    useEffect(() => {
+        let textSize =calculateSize(textAux,"Arial")+60
+        let elementSize = textEl.current.clientWidth
+        let numberCharOverflow = Math.floor( textSize - elementSize )
+        if(numberCharOverflow==0) numberCharOverflow=1
+        if( textSize > elementSize ){
+            let textLength = textAux.length
+            let index = textLength-(numberCharOverflow)
+
+            let newText = textAux.substring(0, index) + "..."
+
+            if(newText.length<=11) newText=textAux.substring(0,8)+"..."
+            
+            if(elementSize<66) newText=""
+
+            setText(newText)
+        }else{
+            setText(textAux)
+        }
+    })
+    
+    const calculateSize = (text,font) => {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+
+        context.font = font || getComputedStyle(document.body).font;
+
+        return context.measureText(text).width;
+    }
   return (
     <>
     <nav>
@@ -44,23 +78,25 @@ function Nav() {
                 
             </div>
         </div>
-        <div id="nav-text-section" className="nav-section">
-            <p>Nav.jsx - portafolio - Visual Studio Code</p>
+        <div id="nav-text-section" ref={textEl} className="nav-section text">
+            <p>{text}</p>
         </div>
-        <div id="nav-other-section" className="nav-section">
-                <a href="#">
-                    <VscSplitHorizontal />
-                </a>
-                <a href="#">
-                    <VscSplitVertical />
-                </a>
-                <a href="#">
-                    <VscSplitHorizontal />
-                </a>
-                <a href="#">
-                    <VscTerminalTmux />
-                </a>
-        </div>
+        {size.width>=300 ? (
+            <div id="nav-other-section" className="nav-section">
+                    <a href="#">
+                        <VscSplitHorizontal />
+                    </a>
+                    <a href="#">
+                        <VscSplitVertical />
+                    </a>
+                    <a href="#">
+                        <VscSplitHorizontal />
+                    </a>
+                    <a href="#">
+                        <VscTerminalTmux />
+                    </a>
+            </div>
+        ):(<></>)}
         <div id="nav-button-section" className="nav-section">
             <a href="#">
                 <VscChromeMinimize />
